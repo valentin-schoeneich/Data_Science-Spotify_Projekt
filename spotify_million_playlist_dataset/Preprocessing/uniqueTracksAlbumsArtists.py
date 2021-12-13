7e271afa991cec58dd391b9bd08a43c26569e459
valentin-schoeneich <93647776+valentin-schoeneich@users.noreply.github.com>
2021-12-13 10:29:48 +0100

    Update uniqueTracksAlbumsArtists.py

--- spotify_million_playlist_dataset/Preprocessing/uniqueTracksAlbumsArtists.py
+++ spotify_million_playlist_dataset/Preprocessing/uniqueTracksAlbumsArtists.py
@@ -2,24 +2,15 @@ import csv
 import pandas as pd
 
 
-def makeTracksUnique():
-    tracks = pd.read_csv("tracks_slice.csv")
-    # print(tracks)
-    tracks = tracks.drop_duplicates()
-    print("Unique tracks ", tracks.shape[0])
+def makeCSVUnique(filename):
+    print('reading ' + filename)
+    df = pd.read_csv(filename + '.csv')
+    print('dropping duplicates in ' + filename)
+    df = df.drop_duplicates()
+    print('writing to csv')
+    df.to_csv(filename + '_unique.csv')
 
-def makeAlbumsUnique():
-    albums = pd.read_csv("album_slice.csv")
-    # print(albums)
-    albums = albums.drop_duplicates('album_uri')
-    print("Unique albums ", albums.shape[0])
 
-def makeArtistUnique():
-    artists = pd.read_csv("artist_slice.csv")
-    # print(artists)
-    artists = artists.drop_duplicates('artist_uri')
-    print("Unique artists ", artists.shape[0])
-
-makeTracksUnique()
-makeAlbumsUnique()
-makeArtistUnique()
+makeCSVUnique('albums')
+makeCSVUnique('tracks')
+makeCSVUnique('artists')
