50ec216f704e3cff3fe576e720bdf8963a514296
valentin-schoeneich <valentin.schoeneich@student.hs-rm.de>
2022-01-24 11:09:36 +0100

    apriori spotify

--- spotify_million_playlist_dataset/src/dbconnect.py
+++ spotify_million_playlist_dataset/src/dbconnect.py
@@ -1,20 +1,26 @@
 import psycopg2 as psycopg2
-try:
-    conn = psycopg2.connect(
-        host="localhost",
-        database="ldude001",
-        user="ldude001",
-        password="ldude001",
-        port="9001",
-    )
-    cur = conn.cursor()
-    print('PostgreSQL database version:')
-    cur.execute('SELECT * FROM albums')
-    print(cur.fetchall())
-    cur.close()
-except (Exception, psycopg2.DatabaseError) as error:
+
+
+def dbReq(query):
+    tracks_playlists = -1
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="ldude001",
+            user="ldude001",
+            password="ldude001",
+            port="9001",
+        )
+        cur = conn.cursor()
+
+        print('PostgreSQL database version:')
+        cur.execute(query)
+        tracks_playlists = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-finally:
-    if conn is not None:
-        conn.close()
-        print('Database connection closed.')
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+    return tracks_playlists
