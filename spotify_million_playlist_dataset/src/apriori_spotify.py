a1fe413a396abd0150269ba2c679438b86c5eaa3
valentin-schoeneich <valentin.schoeneich@student.hs-rm.de>
2022-01-24 12:17:11 +0100

    apriori spotify

--- spotify_million_playlist_dataset/src/apriori_spotify.py
+++ spotify_million_playlist_dataset/src/apriori_spotify.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import chain, combinations
 
 from dbconnect import *
 
@@ -23,14 +24,14 @@ def getFromDB(maxPlaylists):
 # likelihood of a track being included in a playlist
 # e.g. for minSup = 0.5 this function returns all tracks which are
 #       included in a playlist with a likelihood of 50%
-def getAboveMinSup(unique_tracks, playlists, minSup, globalItemSetWithSup):
+def getAboveMinSup(unique_tracks, playlists, minSup, globalTrackSetWithSup):
     freqTrackSet = set()
     localTrackSetWithSup = defaultdict(int)
 
     for track in unique_tracks:
         for playlistSet in playlists:
             if track.issubset(playlistSet):
-                globalItemSetWithSup[track] += 1
+                globalTrackSetWithSup[track] += 1
                 localTrackSetWithSup[track] += 1
 
     for track, supCount in localTrackSetWithSup.items():
@@ -42,21 +43,79 @@ def getAboveMinSup(unique_tracks, playlists, minSup, globalItemSetWithSup):
 
 
 
-def aprioriFromFile(minSup = 0.5, minConf = 0.5):
-    unique_tracks, playlists = getFromDB(1)
-    print(unique_tracks)
-    print(playlists)
+def getUnion(trackSet, length):
+    return set([i.union(j) for i in trackSet for j in trackSet if len(i.union(j)) == length])
+
+
+
+def pruning(candidateSet, prevFreqSet, length):
+    tempCandidateSet = candidateSet.copy()
+    for item in candidateSet:
+        subsets = combinations(item, length)
+        for subset in subsets:
+            # if the subset is not in previous K-frequent get, then remove the set
+            if(frozenset(subset) not in prevFreqSet):
+                tempCandidateSet.remove(item)
+                break
+    return tempCandidateSet
+
+
+
+def powerset(s):
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)))
+
+
+
+def associationRule(freqItemSet, itemSetWithSup, minConf):
+    rules = []
+    for k, itemSet in freqItemSet.items():
+        for item in itemSet:
+            subsets = powerset(item)
+            for s in subsets:
+                confidence = float(
+                    itemSetWithSup[item] / itemSetWithSup[frozenset(s)])
+                if(confidence > minConf):
+                    rules.append([set(s), set(item.difference(s)), confidence])
+    return rules
+
+
+
+def aprioriFromDB(numberPlaylists, minSup = 0.5, minConf = 0.5):
+    unique_tracks, playlists = getFromDB(numberPlaylists)
+    #print(unique_tracks)
+    #print(playlists)
 
     # Final result global frequent itemset
-    globalFreqItemSet = dict()
+    globalFreqTrackSet = dict()
     # Storing global itemset with support count
-    globalItemSetWithSup = defaultdict(int)
+    globalTrackSetWithSup = defaultdict(int)
 
     L1ItemSet = getAboveMinSup(
-        unique_tracks, playlists, minSup, globalItemSetWithSup)
+        unique_tracks, playlists, minSup, globalTrackSetWithSup)
     currentLSet = L1ItemSet
     k = 2
-    print(currentLSet)
+    #print(currentLSet)
+
+    # Calculating frequent track set
+    while (currentLSet):
+        # Storing frequent itemset
+        globalFreqTrackSet[k - 1] = currentLSet
+        # Self-joining Lk
+        candidateSet = getUnion(currentLSet, k)
+        # Perform subset testing and remove pruned supersets
+        candidateSet = pruning(candidateSet, currentLSet, k - 1)
+        # Scanning itemSet for counting support
+        currentLSet = getAboveMinSup(
+            candidateSet, playlists, minSup, globalTrackSetWithSup)
+        k += 1
+
+    rules = associationRule(globalFreqTrackSet, globalTrackSetWithSup, minConf)
+    rules.sort(key=lambda x: x[2])
+
+    return globalFreqTrackSet, rules
 
 
-aprioriFromFile(0.2, 0.5)
+
+globalFreqTrackSet, rules = aprioriFromDB(20, 0.1, 0.5)
+print(globalFreqTrackSet)
+print(rules)
