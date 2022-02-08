8d0a82c8122fa3ecd4c9cf2c0083b0a95e735867
Louis010301 <83017411+Louis010301@users.noreply.github.com>
2022-02-08 21:30:01 +0100

    file db.py: - methoden generisch gehalten -> Abfrage funktioniert jetzt für 'track_uri', 'track_name', 'artist_uri', 'album_uri' oder 'name' - methoden wurden ausgelagert -> die methoden getL1ItemSet2Pids & getL1Pid2ItemSets sind jetzt deutlich kompakter weil subquerys in andere mehoden ausgelagert wurden
    apriori_spotify.py
    - getNextItemSets (vorher getNextTrackSet) funktioniert jetzt generisch für verschiedene items und wurde zusätzlich ausgelagert in proveCandidates()
    - aprioriAdvanced kann jetzt für beliebges k itemSets erstellen
    
    progressBar.py
    - Zeigt den aktuellen Status des Prozesses an und berechnet immer wieder die noch ausstehende Laufzeit des Programms durch die gelaufene Laufzeit
    - Zeigt am Ende des Prozesses die gesamte Laufzeit an und kann so zu diagnose-Zwecken verwendet werden

--- spotify_million_playlist_dataset/src/apriori_spotify.py
+++ spotify_million_playlist_dataset/src/apriori_spotify.py
@@ -1,18 +1,22 @@
 import itertools
+import time
 from collections import defaultdict
 from itertools import chain, combinations
 
 from db import *
+from progressBar import *
 
 
-# load variable number of playlists from our database
-# returns unique tracks of each playlist and unique tracks of all playlist
-
-
-# likelihood of a track being included in a playlist
-# e.g. for minSup = 0.5 this function returns all tracks which are
-#       included in a playlist with a likelihood of 50%
 def getAboveMinSup(unique_tracks, playlists, minSup, globalTrackSetWithSup):
+    """
+    :param unique_tracks:
+    :param playlists:
+    :param minSup:  Likelihood of a track being included in a playlist
+                    e.g. for minSup = 0.5 this function returns all tracks which are included in a playlist
+                    with a likelihood of 50%
+    :param globalTrackSetWithSup:
+    :return:
+    """
     freqTrackSet = set()
     localTrackSetWithSup = defaultdict(int)
     i = 0
@@ -41,51 +45,6 @@ def getUnion(trackSet, length):
                 returns: {{A,B}, {A,C}, {A,D}, {B,C}, {B,D}, {C,D}}
     '''
     return set([i.union(j) for i in trackSet for j in trackSet if len(i.union(j)) == length])
-
-
-def getNextTrackSet(playlistsDict, tracksDict, minSupPercent, maxPlaylists, k):
-    '''
-    Combines getAboveMinSup() and getUnion() and is also way faster.
-    :param playlistsDict:   This parameter is used to iterate over the playlists of a track to
-                            concatenate it only with tracks from playlists itself appears in.
-                            Due to an average occurrence of 5 playlists per track it reduces the number of candidates
-                            significantly
-                            This parameter is also used to get the support of a track by counting the playlists
-    :param k:               Size of trackSet
-    :param minSupPercent:   Specifies the minimum number of playlists the trackSet must appear in
-    :param tracksDict:      This parameter is necessary to get the tracks above minSup of a playlist, so that
-                            we don't have to iterate over the entire playlist
-    :return:    playlistsDict and tracksDict of next trackSet in form of:
-                tracksDict:
-                {
-                    pid: {frozenset({'track_uri', 'track_uri', ...}), ...}
-                    5: {frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1', ...}),
-                        frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4', ...})},
-                    0: {frozenset({'spotify:track:3H1LCvO3fVsK2HPguhbml0', ...}),
-                        frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4', ...})},
-                    ...
-                }
-                playlistsDict:
-                 {
-                    frozenset({'track_uri', 'track_uri', ...}): {pid, pid, ...},
-                    frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1', ...}): {0, 5},
-                    frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4', ...}): {0, 5},
-                    ...
-                }
-    '''
-    minSup = maxPlaylists * minSupPercent  # the actual number of playlists the tracks must appears in
-    candidates = set()
-    for trackSet1 in playlistsDict.keys():
-        for tS1playlist in playlistsDict.get(trackSet1):
-            for trackSet2 in tracksDict.get(tS1playlist):
-                tS1Playlists = playlistsDict.get(trackSet1)
-                tS2Playlists = playlistsDict.get(trackSet2)
-                supplyOfBoth = len(tS1Playlists.intersection(tS2Playlists))
-                if supplyOfBoth >= minSup:  # both tracks are together in more or equal than minSup playlists
-                    candidate = frozenset(set([trackSet1]).union(set([trackSet2])))
-                    if len(candidate) == k:
-                        candidates.add(candidate)
-    return candidates
 
 
 def pruning(candidateSet, prevFreqSet, length):
@@ -181,15 +140,130 @@ def aprioriFromDB(maxPlaylists, minSup, minConf=0.5, kMax=2):
     return globalFreqTrackSet, rules
 
 
-def aprioriAdvanced(maxPlaylists, minSup, minConf=0.5, kMax=2):
-    # get for each playlist a set tracks above minSup
-    l1TracksDict = getL1TracksDict(maxPlaylists, minSup)
-    l1PlaylistsDict = getL1PlaylistsDict(maxPlaylists, minSup)
-    print(len(l1PlaylistsDict))
-    l2TrackSet = getNextTrackSet(l1PlaylistsDict, l1TracksDict, minSup, maxPlaylists, 2)
-    print(len(l2TrackSet))
+'''
+***************************************************************
+Methods for new approach
+***************************************************************
+'''
 
 
-aprioriFromDB(50, 0.04)
-print("*"*80)
-aprioriAdvanced(50, 0.04)
+def getNextTracksDict(currPid2ItemSets, nextItemSet2Pids):
+    nextPid2ItemSets = dict()
+    for pid in currPid2ItemSets:
+        nextPid2ItemSets[pid] = set()
+
+    for itemSet in nextItemSet2Pids:
+        for pid in nextItemSet2Pids[itemSet]:
+            nextPid2ItemSets[pid].add(itemSet)
+
+    return nextPid2ItemSets
+
+
+def getNextItemSets(currItemSet2Pids, currPid2ItemSets, minSupPercent, maxPid, k, b):
+    """"
+    Combines getAboveMinSup() and getUnion() and is also way faster for k = 2.
+    :param currItemSet2Pids:    This parameter is used to iterate over the playlists of a itemSet to
+                                concatenate it only with itemSets from playlists itself appears in.
+                                Due to an average occurrence of 10 playlists per track it reduces the number of
+                                candidates significantly
+                                This parameter is also used to get the support of a itemSet by counting the playlists
+
+    :param currPid2ItemSets:    This parameter is necessary to get the itemSets above minSup of a playlist, so that
+                                 we don't have to iterate over the entire playlist
+    :param minSupPercent:   Specifies the minimum number of playlists the itemSet must appear in
+    :param maxPid:          Number of playlists that limits the load of data. Can be used for faster testing.
+    :param k:               Size of itemSet
+    :param b:               Number of b - nextItemSets with highest support that should be saved
+                            Standard value of b is -1. If b is -1 all itemSets above minSUp with length k will be saved.
+                            With b != -1 the program isn't deterministic
+    :return:    pid2Itemsets and itemSet2Pids for k-itemSets in form of:
+                pid2Itemsets:
+                {
+                    pid: {frozenset({'item', 'item', ...}), ...}
+                    ...
+                }
+                e.g. with item = track_uri and k = 2:
+                    5: {frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1', 'spotify:track:0XUfyU2QviPAs6bxSpXYG4'}),
+                        frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4', 'spotify:track:3H1LCvO3fVsK2HPguhbml0'})
+                        ...},
+                    0: {frozenset({'spotify:track:3H1LCvO3fVsK2HPguhbml0', 'spotify:track:0XUfyU2QviPAs6bxSpXYG4'}),
+                        frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4', 'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'})
+                        ...},
+                    ...
+                }
+                itemSet2Pids
+                {
+                    frozenset({'item', 'item', ...}): {pid, pid, ...},
+                    ...
+                }
+                e.g. with item = track_uri and k = 2:
+                    frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1',
+                    'spotify:track:3H1LCvO3fVsK2HPguhbml0'}): {0, 5, ...},
+                    frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4',
+                    'spotify:track:3H1LCvO3fVsK2HPguhbml0'}): {0, 5, ...},
+                    ...
+                }
+    """
+    minSup = maxPid * minSupPercent  # the actual number of playlists the item must appears in
+    nextItemSet2Pids = dict()
+    countProgress = 0
+    pb = ProgressBar(total=len(currItemSet2Pids), prefix='Calculate nextItemSet2Pids', timing=True)
+    for itemSet1 in currItemSet2Pids:  # for each k-1 itemSet above minSup
+        pb.printProgressBar(countProgress)
+        proveCandidates(itemSet1, b, k, minSup, currItemSet2Pids, currPid2ItemSets, nextItemSet2Pids)
+        countProgress += 1
+    # for better performance in next round we need a dict that gives us for each playlist the k-itemSets above minSup
+    nextPid2ItemSets = getNextTracksDict(currPid2ItemSets, nextItemSet2Pids)
+
+    return nextItemSet2Pids, nextPid2ItemSets
+
+
+def proveCandidates(itemSet1, b, k, minSup, currItemSet2Pids, currPid2ItemSets, nextItemSet2Pids):
+    candidates = [
+        (frozenset(itemSet1.union(itemSet2)), currItemSet2Pids[itemSet1].intersection(currItemSet2Pids[itemSet2]))
+        for iS1Pid in currItemSet2Pids[itemSet1]  # for each playlist the k-1 itemSet appears in
+        for itemSet2 in currPid2ItemSets[iS1Pid]  # for each k-1 itemSet above minSup in playlist
+        # itemSet1 & itemSet2 must be together in min. minSup playlists
+        if len(currItemSet2Pids[itemSet1].intersection(currItemSet2Pids[itemSet2])) >= minSup
+           and len(itemSet1.union(itemSet2)) == k]
+    # on this point the candidate is proved for minSup and length so it can be used for rules
+    # only save the best b candidates with highest support
+    candidates.sort(key=takeSupport, reverse=True)
+    for i in range(0, b + 1 if b < len(candidates) and b != -1 else len(candidates)):
+        nextItemSet2Pids[candidates[i][0]] = candidates[i][1]
+    candidates.clear()
+
+
+def takeSupport(elem):
+    return len(elem[1])
+
+
+def aprioriAdvanced(item, maxPid, minSup, minConf=0.5, kMax=2, b=-1):
+    # get for each playlist a set of items above minSup
+    currentPid2ItemSets = getL1Pid2ItemSets(item, maxPid, minSup)
+    # get for each item a list of playlists it appears in
+    currentItemSet2Pids = getL1ItemSet2Pids(item, maxPid, minSup)
+    print(f"k: 1 -> {len(currentItemSet2Pids)} items "
+          f"({round(len(currentItemSet2Pids) / getNumUniqueItems(item, maxPid) * 100, 2)}%)")
+    for k in range(2, kMax + 1):
+        currentItemSet2Pids, currentPid2ItemSets = getNextItemSets(currentItemSet2Pids, currentPid2ItemSets, minSup,
+                                                                   maxPid, k, b)
+        print("k: ", k, "->", len(currentItemSet2Pids), "itemSets")
+    sup = 0
+
+    items = set()
+    for key in currentItemSet2Pids:
+        sup += len(currentItemSet2Pids[key])
+        for i in key:
+            items.add(i)
+
+    print("AVG Supply:", round(sup / max(1, len(currentItemSet2Pids)), 2))
+    print(f"({round(len(items) / getNumUniqueItems(item, maxPid) * 100, 2)}%)")
+
+    # print(currentItemSet2Pids)
+
+
+# aprioriFromDB(100, 0.02, kMax=3)
+# aprioriFromDB(200, 0.01)
+print("*" * 80)
+aprioriAdvanced('track_uri', 40000, 0.00005, b=5)
--- spotify_million_playlist_dataset/src/db.py
+++ spotify_million_playlist_dataset/src/db.py
@@ -1,34 +1,12 @@
 import psycopg2 as psycopg2
-from enum import Enum
-
-
-class _Query(Enum):
-    MostPopularTracks = "SELECT track_uri, num_tracks " \
-                        "FROM " \
-                            "(SELECT track_uri, COUNT(track_uri) AS num_tracks " \
-                            "FROM " \
-                                "(SELECT track_uri " \
-                                "FROM pcont " \
-                                "GROUP BY track_uri, pid) AS x " \
-                            "GROUP BY track_uri) AS y " \
-                        "WHERE num_tracks >= 2 " \
-                        "ORDER BY num_tracks DESC "
-    NumPlaylists = "SELECT COUNT(pid) FROM playlists"
-    NumTracks = "SELECT COUNT(id) FROM pcont"
-    NumAvgPlaylistLength = "SELECT AVG(x.num_tracks) " \
-                           "FROM (SELECT pid, COUNT(track_uri) AS num_tracks FROM pcont GROUP BY pid) AS x"
-    NumUniqueTracks = "SELECT COUNT(track_uri) FROM tracks"
-    NumUniqueTitles = "SELECT COUNT(track_name) FROM tracks"
-    NumUniqueAlbums = "SELECT COUNT(album_uri) FROM albums"
-    NumUniqueArtists = "SELECT COUNT(artist_uri) FROM artists"
 
 
 def _dbReq(query):
-    '''
+    """
     This method is used by all our db-requests. It is private, so that all sql-query's are collected in this file
     :param query: Can be a single sql-query or a list of query's for multiple db-requests with single connection
     :return: Returns the unfiltered output from the database
-    '''
+    """
     try:
         conn = psycopg2.connect(
             host="localhost",
@@ -59,20 +37,57 @@ def _dbReq(query):
     return output
 
 
-def getFromDB(maxPlaylists):
-    '''
+def _getQuerySupportOfItems(item, maxPid):
+    """
+    The output of the query (not this method) is the support and a list of playlists for each item from a playlist
+    with pid < maxPid.
+    :param item:    Could be either 'track_uri', 'track_name', 'artist_uri', 'album_uri' or 'name' (name of playlist)
+    :param maxPid:  Maximum size of playlists for where-clause
+    :return:        String, that can be used for a sql-query
+    """
+    _validateItem('getQuerySupportOfItems', item)
+    query = f"SELECT {item}, COUNT({item}) AS support, string_agg(pid::character varying, ',') AS pids " \
+            f"FROM ({_getQueryUniqueItemsOfPlaylists(item, maxPid)}) AS x GROUP BY {item}"
+    return query
+
+
+def _getQueryUniqueItemsOfPlaylists(item, maxPid):
+    """
+    The output of the query (not this method) is a list of (item, pid)-pairs. The query can be used to calculate the
+    support in a further query without counting duplicates inside a playlist.
+    :param item:    Could be either 'track_uri', 'track_name', 'artist_uri', 'album_uri' or 'name' (name of playlist)
+    :param maxPid:  Maximum size of playlists for where-clause
+    :return:        String, that can be used for a sql-query
+    """
+    _validateItem('getQueryUniqueItemsOfPlaylists', item)
+    if item == 'track_uri':
+        query = f"SELECT {item}, pid FROM pcont WHERE pid < {maxPid} GROUP BY {item}, pid"
+    elif item == 'name':
+        query = f"SELECT {item}, pid FROM playlists WHERE pid < {maxPid} GROUP BY {item}, pid"
+    else:
+        query = f"SELECT {item}, pid FROM pcont NATURAL JOIN tracks WHERE pid < {maxPid} GROUP BY {item}, pid"
+    return query
+
+
+def _validateItem(name, item):
+    validItem = {'track_uri', 'track_name', 'artist_uri', 'album_uri', 'name'}
+    if item not in validItem:
+        raise ValueError(name, ": item must be one of %r." % validItem)
+
+
+def getFromDB(maxPid):
+    """
     This method was used for our first apriori-attempt. We didn't use it anymore because it returns to much data
     and doesn't offer the possibility of quickly finding new candidates with the playlists of a track.
-    :param maxPlaylists: Number of playlists that limits the load of data.
-    :return:    Returns a set of all tracks from playlists with pid < maxPlaylists
-                and a list of track-sets for each playlist
-    '''
+    :param      maxPid: Number of playlists that limits the load of data.
+    :return:    Returns unique tracks of each playlist and unique tracks of all playlist
+    """
     playlists = []
     unique_tracks = set()
     playlistCounter = 0
     record = set()
 
-    tracks = _dbReq(f'{"SELECT track_uri, pid FROM pcont WHERE pid<"}{maxPlaylists}')
+    tracks = _dbReq(f'{"SELECT track_uri, pid FROM pcont WHERE pid<"}{maxPid}')
     for track in tracks:
         if playlistCounter != track[1]:
             playlistCounter += 1
@@ -85,116 +100,130 @@ def getFromDB(maxPlaylists):
     return unique_tracks, playlists
 
 
-def getL1PlaylistsDict(maxPlaylists, minSupPercent):
-    '''
-    This method generates the l1TrackSet by a database request, so the program doesn't need
-    to work with tracks under the minSup. It saves memory and is faster than a method that filters the tracks.
-    It can be used to iterate over the playlists of a track or getting the supply by calculate the length of the
-    playlist-set.
-
-    :param maxPlaylists:    Number of playlists that limits the load of data. Can be used for faster testing
+def getL1ItemSet2Pids(item, maxPid, minSupPercent):
+    """
+    This method generates the l1ItemSet by a database request, so the program doesn't need
+    to work with items under the minSup. It saves memory and is faster than a method that filters the items only with
+    maxPid.
+    It can be used to iterate over the playlists of a itemSet or getting the supply by calculate the length of the
+    pid-set.
+    :param item:            Could be either 'track_uri', 'track_name', 'artist_uri', 'album_uri'
+                            or 'name' (name of playlist)
+    :param maxPid:          Number of playlists that limits the load of data. Can be used for faster testing
                             or to free up the memory
     :param minSupPercent:   Specifies the minimum number of playlists the track must appear in
-    :return:    A dictionary that assigns each track above minSup the playlists it appears in.
+    :return:    A dictionary that lists for each itemSet above minSup the playlists it appears in. In this case
+                of l1ItemSet, every itemSet have the same length = 1
                 The return value of the dictionary is of the form:
                 {
-                    frozenset({'track_uri'}): {pid, pid, ...},
-                    frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'}): {0, 5},
-                    frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4'}): {0, 5},
+                    frozenset({'item'}): {pid, pid, ...},
                     ...
                 }
-    '''
-    minSup = maxPlaylists * minSupPercent   # the actual number of playlists the tracks must appears in
-    l1PlaylistsDict = dict()     # the return value of this method
-    resp = _dbReq("SELECT track_uri, support, pids "
-                      "FROM "
-                        "(SELECT track_uri, "
-                                "COUNT(track_uri) AS support, "     # count in how many playlists the song appears
-                                "string_agg(pid::character varying, ',') AS pids "    
-                         "FROM "                                                     
-                            "(SELECT track_uri, pid "    # some tracks are duplicated in a playlists
-                            "FROM pcont "           # remove these duplicates with GROUP BY track_uri, pid 
-                            "WHERE pid <" + str(maxPlaylists) +
-                            " GROUP BY track_uri, pid) AS x "
-                        "GROUP BY track_uri) AS y "
-                      "WHERE support >=" + str(minSup) +
-                      " ORDER BY support DESC")
+                e.g.
+                {
+                    frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'}): {0, 5, 2},
+                    frozenset({'spotify:track:0XUfyU2QviPAs6bxSpXYG4'}): {1, 3, 5}
+                }
+    """
+    _validateItem('getL1ItemSet2Pids', item)
+    minSup = maxPid * minSupPercent  # the actual number of playlists the item must appears in
+    l1ItemSet2Pids = dict()  # the return value of this method
+    resp = _dbReq(f"SELECT {item}, support, pids "
+                  f"FROM ({_getQuerySupportOfItems(item, maxPid)}) AS x "
+                  f"WHERE support >= {minSup}")
 
     # formatting the response in a dictionary
     for line in resp:
-        track_uri = frozenset([line[0]])  # track_uri of a track above minSup
-        playlists = str(line[2])  # list of playlists in form of '5,1,0,12,...'
-        l1PlaylistsDict[track_uri] = set(map(int, playlists.split(',')))
+        item = frozenset([line[0]])
+        pids = str(line[2])  # list of playlists in form of '5,1,0,12,...'
+        l1ItemSet2Pids[item] = set(map(int, pids.split(',')))
+
+    return l1ItemSet2Pids
 
 
-
-    return l1PlaylistsDict
-
-
-def getL1TracksDict(maxPlaylists, minSupPercent):
-    '''
+def getL1Pid2ItemSets(item, maxPid, minSupPercent):
+    """
     This method is used for a faster generation of candidates.
 
-    :param maxPlaylists:    Number of playlists that limits the load of data. Can be used for faster testing
+    :param item:            Could be either 'track_uri', 'track_name', 'artist_uri', 'album_uri'
+                            or 'name' (name of playlist)
+    :param maxPid:          Number of playlists that limits the load of data. Can be used for faster testing
                             or to free up the memory
-    :param minSupPercent:   Specifies the minimum number of playlists the track must appear in
-    :return:    A dictionary that assigns each playlist the tracks above minSup.
+    :param minSupPercent:   Specifies the minimum number of playlists the item must appear in
+    :return:    A dictionary that lists for each playlist the itemSets above minSup. In this case of l1ItemSet,
+                every itemSet have the same length = 1
                 The return value of the dictionary is of the form:
                 {
-                    pid:    {frozenset({'track_uri', 'track_uri', ...}),
-                            frozenset({'track_uri', 'track_uri', ...}), ...},
-                    5:      {frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1', ...}),
-                            frozenset({'spotify:track:3H1LCvO3fVsK2HPguhbml0'}), ...},
-                    0:      {frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1', ...}),
-                            frozenset({'spotify:track:3H1LCvO3fVsK2HPguhbml0'}), ...},
+                    pid:    {frozenset({'item'}), frozenset({'item'}), ...},
                     ...
                 }
-    '''
-    minSup = maxPlaylists * minSupPercent   # the actual number of playlists the tracks must appears in
-    l1TracksDict = dict()     # the return value of this method
-    resp = _dbReq("SELECT pid, string_agg(x.track_uri, ',')"
-                            " FROM "
-                                "(SELECT track_uri, pid FROM pcont WHERE pid <" + str(maxPlaylists) + "GROUP BY track_uri, pid) AS x"
-                                " INNER JOIN "
-                                "(SELECT track_uri, COUNT(track_uri) AS support"
-                                    " FROM "
-                                        "(SELECT track_uri FROM pcont WHERE pid < " + str(maxPlaylists) + "GROUP BY track_uri, pid) AS z"
-                                    " GROUP BY track_uri"
-                                ") AS y"
-                                " ON x.track_uri = y.track_uri"
-                        " WHERE support >= " + str(minSup) +
-                        " GROUP BY pid")
+                e.g
+                {
+                    5:      {frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'}),
+                            frozenset({'spotify:track:3H1LCvO3fVsK2HPguhbml0'})},
+                    0:      {frozenset({'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'}),
+                            frozenset({'spotify:track:3H1LCvO3fVsK2HPguhbml0'})},
+                }
+    """
+    _validateItem('getL1Pid2ItemSets', item)
+    minSup = maxPid * minSupPercent  # the actual number of playlists the item must appears in
+    l1Pid2ItemSets = dict()  # the return value of this method
+    resp = _dbReq(f"SELECT pid, string_agg(x.{item}, ',') "
+                  f"FROM ({_getQueryUniqueItemsOfPlaylists(item, maxPid)}) AS x "
+                  f"NATURAL JOIN ({_getQuerySupportOfItems(item, maxPid)}) AS y "
+                  f"WHERE support >= {minSup} "
+                  f"GROUP BY pid")
     # formatting the response in a dictionary
     for line in resp:
         pid = line[0]  # pid of playlist
-        tracks = str(line[1]).split(',')  # line[1] in form 'track_uri_1,track_uri_2,...'
-        l1TracksDict[pid] = set()
-        for track in tracks:
-            l1TracksDict[pid].add(frozenset([track]))
+        items = str(line[1]).split(',')  # line[1] in form 'item1,item2,...'
+        l1Pid2ItemSets[pid] = set()
+        for item in items:
+            l1Pid2ItemSets[pid].add(frozenset([item]))
 
-    return l1TracksDict
+    return l1Pid2ItemSets
+
+
+def getNumUniqueItems(item, maxPid):
+    """
+    To calculate the percentage of items for which a rule was created, you have to know how many items exists in total.
+    This method helps for this calculation by giving the total number of unique appearances of a item
+    depended on maxPid.
+    :param item:    Could be either 'track_uri', 'track_name', 'artist_uri', 'album_uri' or 'name' (name of playlist)
+    :param maxPid:  Maximum size of playlists for where-clause
+    :return:    Returns the number of appearances of a item in a range given by maxPid. The appearances are counted
+                unique over all playlists with pid < maxPid
+    """
+    _validateItem('getNumUniqueItems', item)
+    if item == 'track_uri':
+        resp = _dbReq(f"SELECT COUNT({item}) "
+                      f"FROM (SELECT {item} FROM pcont WHERE pid < {maxPid} GROUP BY {item}) AS x")
+    elif item == 'name':
+        resp = _dbReq(f"SELECT COUNT({item}) "
+                      f"FROM (SELECT {item} FROM playlists WHERE pid < {maxPid} GROUP BY {item}) AS x")
+    else:
+        resp = _dbReq(f"SELECT COUNT({item}) "
+                      f"FROM (SELECT {item} FROM pcont NATURAL JOIN tracks WHERE pid < {maxPid} GROUP BY {item}) AS x")
+    return int(str(resp)[2:-3])
 
 
 def dbValidate():
-    '''
+    """
     This method prints infos about our database that can be compared with the project stats
     :return:    Doesnt return anything. Only prints data.
-    '''
-    resp = _dbReq([_Query.NumPlaylists.value,
-                   _Query.NumTracks.value,
-                   _Query.NumUniqueTracks.value,
-                   _Query.NumUniqueAlbums.value,
-                   _Query.NumUniqueArtists.value,
-                   _Query.NumUniqueTitles.value,
-                   _Query.NumAvgPlaylistLength.value])
+    """
+    NumUniqueTracks = "SELECT COUNT(track_uri) FROM tracks"
+    NumUniqueTitles = "SELECT COUNT(name) FROM (SELECT name FROM playlists GROUP BY(name)) AS x"
+    NumUniqueAlbums = "SELECT COUNT(album_uri) FROM albums"
+    NumUniqueArtists = "SELECT COUNT(artist_uri) FROM artists"
+    NumPlaylists = "SELECT COUNT(pid) FROM playlists"
+    NumTracks = "SELECT COUNT(id) FROM pcont"
+    resp = _dbReq([NumPlaylists, NumTracks, NumUniqueTracks, NumUniqueAlbums, NumUniqueArtists, NumUniqueTitles])
     print("number of playlists", str(resp[0])[2:-3])
     print("number of tracks", str(resp[1])[2:-3])
     print("number of unique tracks", str(resp[2])[2:-3])
     print("number of unique albums", str(resp[3])[2:-3])
     print("number of unique artists", str(resp[4])[2:-3])
     print("number of unique titles", str(resp[5])[2:-3])
-    print("avg playlist length", str(resp[6])[11:-5])
-
-
 
 
